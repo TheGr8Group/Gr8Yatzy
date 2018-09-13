715099dd17158f96d80f99f76938b808c13733f4
Annica Alienus <anbj1001@student.miun.se>
2018-09-13 14:27:38 +0200

    Added class Dice, Player, Protocol and Yatzy

---
+++ Gr8Yatzy/Gr8Yatzy/Protocol.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gr8Yatzy
+{
+    enum YatzyValues
+    {
+        Ones, Twos, Threes, Fours, Fives, Sixes, OnePair, TwoPair, ThreeOfAKind, FourOfAKind, SmallStraight, LargeSraight, FullHouse, Chance, Yatzy
+    }
+    class Protocol
+    {
+        public int Ones { get; set; }
+        public int Twos { get; set; }
+        public int Threes { get; set; }
+        public int Fours { get; set; }
+        public int Fives { get; set; }
+        public int Sixes { get; set; }
+        public int UpperSum { get; set; }
+        public int Bonus { get; set; } // Kan ju räkna ut bonusen direk och sätta värdet direkt i Bonus
+
+        public int OnePair { get; set; }
+        public int TwoPair { get; set; }
+        public int ThreeOfAKind { get; set; }
+        public int FourOfAKind { get; set; }
+        public int SmallStraight { get; set; }
+        public int LargeStraight { get; set; }
+        public int FullHouse { get; set; }
+        public int Chance { get; set; }
+        public int Yatzy { get { return 50; } }
+        public int TotalSum { get; set; }
+
+        public void SetValues(List<Dice> savedDices, YatzyValues row)
+        {
+            foreach (Dice d in savedDices)
+            {
+                if (row.Equals(d.Value))// använder enums för att identifiera rad i protokoll
+                {
+                    // använda enumns för att ange vilken int som ges värde.
+
+                }
+            }
+        }
+
+    }
+}
---
+++ Gr8Yatzy/Gr8Yatzy/Yatzy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gr8Yatzy
+{
+    class Yatzy
+    {
+        public int GameID { get; set; }
+        public int Rolls { get; set; }
+        public List<Player> Players { get; set; }// hur sätta ett maxvärde till en lista?
+        public int MaxAlloawedPlayers { get; set; }
+        public int MinAllowedPlayers { get; set; }
+        public List<Dice> Dices { get; set; } // de tärningar som kan kastas
+        public List<Dice> SavedDices { get; set; }// lägger de tärningar som sparas i en separat lista
+
+
+        /// <summary>
+        /// Slumpar fram värdet från de tärningar i listan
+        /// </summary>
+        /// <param name="dices"></param>
+        /// <returns></returns>
+        public List<Dice> RollDices(List<Dice> dices)
+        {
+
+            Random random = new Random();
+
+            foreach (Dice dice in dices)
+            {
+                dice.Value = random.Next(1, 7);
+            }
+
+            return dices;
+        }
+
+
+        /// <summary>
+        /// Tar bort de tärningarna som sparas från dices och lägger till den/de tärningar
+        /// i en lista med sparade. 
+        /// </summary>
+        /// <param name="dices"></param>
+        /// <returns></returns>
+        public List<Dice> SaveDices(List<Dice> dices)// Behöver en funktion för att kunna välja tärning att kastas igen
+        {
+            foreach (Dice d in dices)
+            {
+                if (d.IsSaved == true)
+                {
+                    dices.Remove(d);
+                    SavedDices.Add(d);
+                }
+            }
+            return dices;
+        }
+
+    }
+}
