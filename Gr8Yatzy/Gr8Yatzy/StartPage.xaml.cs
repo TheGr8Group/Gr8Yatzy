27f450986a0cbb1217922210a3df81a2abfe2940
Annica Alienus <anbj1001@student.miun.se>
2018-09-14 16:18:31 +0200

    Lagt till RollDices och ClearArraySAvedDices. Kommenterat ut propertyn Dices i klasssen Yatzy.  Många utkomenterade testvariabler!

--- Gr8Yatzy/Gr8Yatzy/StartPage.xaml.cs
+++ Gr8Yatzy/Gr8Yatzy/StartPage.xaml.cs
@@ -25,50 +25,11 @@ namespace Gr8Yatzy
             InitializeComponent();
         }
 
+        Yatzy yatzy = new Yatzy();
+        
         private void btnTest_Click(object sender, RoutedEventArgs e)
         {
-            //foreach (var item in arr)
-            //{
-            //    dices.Add(new Dice());
-            //}
-
-            //Random random = new Random();
-
-            //foreach (Dice d in dices)
-            //{
-            //    d.Value = random.Next(1, 7);
-            //    //MessageBox.Show(d.Value.ToString());
-            //}
-
-            //Yatzy yatzy = new Yatzy();
-
-            //Random random = new Random();
-            //yatzy.Dices = new int[5];
-
-            //for (int i = 0; i < yatzy.Dices.Length; i++)
-            //{
-            //    yatzy.Dices[i] = random.Next(1, 7);
-            //}
-
-            //foreach (var item in yatzy.Dices)
-            //{
-            //    lstBoxTest.Items.Add(item);
-            //}
-
-            Yatzy yatzy = new Yatzy();
-
-            Random random = new Random();
-            yatzy.Dices = new int[5];
-
-            for (int i = 0; i < yatzy.Dices.Length; i++)
-            {
-                yatzy.Dices[i] = random.Next(1, 7);
-            }
-
-            foreach (var item in yatzy.Dices)
-            {
-                lstPlayers.Items.Add(item);
-            }
+            yatzy.RollDices();
         }
 
         private void btnStartGame_Click(object sender, RoutedEventArgs e)
@@ -81,5 +42,5 @@ namespace Gr8Yatzy
     }
 
 
-    
+
 }
--- Gr8Yatzy/Gr8Yatzy/Yatzy.cs
+++ Gr8Yatzy/Gr8Yatzy/Yatzy.cs
@@ -13,8 +13,59 @@ namespace Gr8Yatzy
         public List<Player> Players { get; set; }// hur sätta ett maxvärde till en lista?
         public int MaxAllowedPlayers { get; set; }
         public int MinAllowedPlayers { get; set; }
-        public int[] Dices { get; set; }
+        //public int[] Dices { get; set; }
 
+        int[] dices = new int[5]; // microsoft rekommenderar inte arrays som propertys. Vi kan göra den som field ist?
+        bool[] savedDices = new bool[5];//test
 
+        /// <summary>
+        /// Kastar 5 tärningar.
+        /// </summary>
+        /// <returns></returns>
+        public int[] RollDices(/*bool[] savedDices*/) // parameter utkommenterad för testnings skull
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < dices.Count(); i++)
+            {
+                if (!savedDices[i])
+                {
+                    dices[i] = random.Next(1, 7);
+                }
+            }
+
+            //För testning
+            //if (1 == 1)
+            //{
+            //    savedDices[0] = true;
+            //    savedDices[1] = true;
+            //    savedDices[2] = false;
+            //    savedDices[3] = false;
+            //    savedDices[4] = true;
+
+            //}
+
+            ClearArraySavedDices(savedDices);
+
+            return dices;
+        }
+
+        /// <summary>
+        /// Sätter savedDices till false inför nästa players omgång
+        /// </summary>
+        /// <param name="savedDices"></param>
+        private void ClearArraySavedDices(bool[] savedDices)
+        {
+            Rolls++;
+
+            if (Rolls == 3)
+            {
+                for (int i = 0; i < savedDices.Count(); i++)
+                {
+                    savedDices[i] = false;
+                }
+                Rolls = 0;
+            }
+        }
     }
 }
