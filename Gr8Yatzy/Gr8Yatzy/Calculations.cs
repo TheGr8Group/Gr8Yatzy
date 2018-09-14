9ff088ee5ddafc8039fb6caaa3fcd4640e7f6612
Giza Admell <giza.admell@gmail.com>
2018-09-14 12:47:26 +0200

    Lagt till class Calculations

---
+++ Gr8Yatzy/Gr8Yatzy/Calculations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gr8Yatzy
+{
+    static class Calculations
+    {
+        static public void CalculateOnes(int points)
+        {
+
+
+        }
+
+    }
+}
