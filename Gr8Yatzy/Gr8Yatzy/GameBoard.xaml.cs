119315b4b9646ba818743cd28bfa2bab67c951cd
Annica Alienus <anbj1001@student.miun.se>
2018-09-14 11:55:18 +0200

    La till en testknapp i main XAML som heter btnTest. Det kan finnas små ändringar i gameboard oxå men det är för att jag la in en knapp och en listbox där först .

--- Gr8Yatzy/Gr8Yatzy/GameBoard.xaml.cs
+++ Gr8Yatzy/Gr8Yatzy/GameBoard.xaml.cs
@@ -24,37 +24,5 @@ namespace Gr8Yatzy
         {
             InitializeComponent();
         }
-
-        private void btnOk_Click(object sender, RoutedEventArgs e)
-        {
-            //foreach (var item in arr)
-            //{
-            //    dices.Add(new Dice());
-            //}
-
-            //Random random = new Random();
-
-            //foreach (Dice d in dices)
-            //{
-            //    d.Value = random.Next(1, 7);
-            //    //MessageBox.Show(d.Value.ToString());
-            //}
-
-
-            Yatzy yatzy = new Yatzy();
-
-            Random random = new Random();
-            yatzy.Dices = new int[5];
-
-            for (int i = 0; i < yatzy.Dices.Length; i++)
-            {
-                yatzy.Dices[i] = random.Next(1, 7);
-            }
-
-            foreach (var item in yatzy.Dices)
-            {
-                lstBoxTest.Items.Add(item);
-            }
-        }
     }
 }
