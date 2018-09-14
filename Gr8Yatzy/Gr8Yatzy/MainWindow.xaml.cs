1a2e18605d6f7bf7212ac607e96e1d81a4d0ced1
Annica Alienus <anbj1001@student.miun.se>
2018-09-14 13:39:09 +0200

    Flyttat RollDices från knappen

--- Gr8Yatzy/Gr8Yatzy/MainWindow.xaml.cs
+++ Gr8Yatzy/Gr8Yatzy/MainWindow.xaml.cs
@@ -25,21 +25,8 @@ namespace Gr8Yatzy
             InitializeComponent();
         }
 
-        private void btnTest_Click(object sender, RoutedEventArgs e)
+        private int[] RollDices()
         {
-            //foreach (var item in arr)
-            //{
-            //    dices.Add(new Dice());
-            //}
-
-            //Random random = new Random();
-
-            //foreach (Dice d in dices)
-            //{
-            //    d.Value = random.Next(1, 7);
-            //    //MessageBox.Show(d.Value.ToString());
-            //}
-
             Yatzy yatzy = new Yatzy();
 
             Random random = new Random();
@@ -50,10 +37,17 @@ namespace Gr8Yatzy
                 yatzy.Dices[i] = random.Next(1, 7);
             }
 
-            foreach (var item in yatzy.Dices)
-            {
-                lstBoxTest.Items.Add(item);
-            }
+            return yatzy.Dices;
+            //foreach (var item in yatzy.Dices)
+            //{
+            //    lstBoxTest.Items.Add(item);
+            //}
+
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            RollDices();
         }
     }
 }
